4b0885a146ae266623bce932b0e7e88753c66573
Nestor <mcclein01@gmail.com>
2020-02-25 09:57:36 -0500

    Ejercicio con error

--- src/main.js
+++ src/main.js
@@ -3,10 +3,42 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+var firebase = require("firebase/app");
+
+// Add additional services that you want to use
+require("firebase/auth");
+// require("firebase/database");
+// require("firebase/firestore");
+// require("firebase/messaging");
+// require("firebase/functions");
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBpn3qlTMIs5834e-OWxdjp1OFkwHVSBE8",
+  authDomain: "crud-udemy-fa34f.firebaseapp.com",
+  databaseURL: "https://crud-udemy-fa34f.firebaseio.com",
+  projectId: "crud-udemy-fa34f",
+  storageBucket: "crud-udemy-fa34f.appspot.com",
+  messagingSenderId: "145619705575",
+  appId: "1:145619705575:web:b531f7b79328c17c8e0c42"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+firebase.auth().onAuthStateChanged((user) => {
+  console.log(user)
+  if (user) {
+    store.dispatch('detectarUsuario', {email: user.email, uid: user.uid})
+  } else {
+    store.dispatch('detectarUsuario', null)
+  }
+
+  new Vue({
+    router,
+    store,
+    render: h => h(App)
+  }).$mount('#app')
+
+})
+
---
+++ src/views/Registro.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h1>Registro de usuarios</h1>
+        <form @submit.prevent="crearUsuario({email, pass1})">
+            <input type="mail" v-model="email" placeholder="Email">
+            <input type="password" v-model="pass1" placeholder="Contraseña">
+            <input type="password" v-model="pass2" placeholder="Repita contraseña">
+            <button type="submit" :disabled="!desactivar">Crear Usuario</button>
+        </form>
+        <p>{{error}}</p>
+    </div>
+</template>
+
+<script>
+import { mapActions, mapState } from "vuex"
+
+export default {
+    name: 'Registro',
+    data() {
+        return {
+            email: '',
+            pass1: '',
+            pass2: ''
+        }
+    },
+    methods: {
+        ...mapActions(['crearUsuario'])
+    },
+    computed: {
+        ...mapState(['error']),
+        desactivar() {
+            return this.pass1 === this.pass2 && this.pass1 != ''
+        }
+    }
+}
+</script>
